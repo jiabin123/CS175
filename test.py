e70fb2d40fc0150e8e521f2f17eb0d35b110d084
ShaodongGu <52988477+ShaodongGu@users.noreply.github.com>
2021-02-09 14:09:53 +0800

    Data Preprocess Tool

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import json
 path = './data/'
 x = []
 
-i = 0
 for f_name in os.listdir('./data'):
     if f_name.endswith('.txt'):
         file = open((path + f_name), 'r')
@@ -12,7 +11,6 @@ for f_name in os.listdir('./data'):
             "rating" : int(f_name[-5]),
             "review" : file.read()
         })
-        i += 1
         file.close()
 
 with open("reviews.json", 'w') as f:
